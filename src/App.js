77dcc22f28a21c9168559b31f03bab10bc797ea0
Keenan Goldis <keenangoldis@gmail.com>
2017-08-10 20:44:15 -0500

    add theWord array

--- src/App.js
+++ src/App.js
@@ -14,30 +14,41 @@ class App extends Component {
 			strikes:0,
 			guess:"",
 			correctGuesses:[],
-			wordToGuessEmptySpaces: []
+			wordToGuessEmptySpaces: [],
+			theWord: []
 		};
 	}
   render() {
 		console.log("this is the length of wordToGuess",this.state.wordToGuess.length, this.state.wordToGuess);
 		let className = `strike-${this.state.strikes}`;
 		let spans = [];
+
 		let emptySpaces = () => {
+			//this function puts an empty space for each letter in wordToGuess.
 			for(let i = 0; i < this.state.wordToGuess.length; i++ ){
 				spans.push(<span> _</span>);
-				console.log("this emptySpaces loop", [i]);
 			}
+		}
 
+		let theWordArray = () => {
+			//this function puts each lette of the wordToGuess in an array.
+			for (let i = 0; i < this.state.wordToGuess.length; i++){
+				this.state.theWord.push(this.state.wordToGuess[i]);
+			}
 		}
 
     return (
 			<div>
 				{emptySpaces()}
+				{theWordArray()}
 
 				<div className="hangman-sprites">
 					<div className={`${className} current`} />
 				</div>
 				<div id="inputs">
 					<div>{spans}</div>
+					<div>{this.state.theWord}</div>
+
 					<input />
 					<button>Guess</button>
 				</div>
